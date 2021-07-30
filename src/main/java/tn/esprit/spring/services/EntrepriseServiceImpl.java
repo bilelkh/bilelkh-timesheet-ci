7a0aaf8cf747980f9d30a11b7618a1c4d32f00c6
bilel <bilel1996k@gmail.com>
2021-07-30 16:48:15 +0100

    add log

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -6,9 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.apache.log4j.Logger;
 
 import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EntrepriseRepository;
@@ -21,53 +21,73 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 	
+	private static final Logger logger = Logger.getLogger(EntrepriseServiceImpl.class);
+	
+	@Override
 	public int ajouterEntreprise(Entreprise entreprise) {
 		entrepriseRepoistory.save(entreprise);
+		logger.info("Entreprise added successfully");
 		return entreprise.getId();
-	}
-
+		
+	}	
+	@Override
 	public int ajouterDepartement(Departement dep) {
 		deptRepoistory.save(dep);
+		logger.info("Departement added successfully");
 		return dep.getId();
 	}
 	
+	@Override
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
-		//Le bout Master de cette relation N:1 est departement  
-				//donc il faut rajouter l'entreprise a departement 
-				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
-				//Rappel : la classe qui contient mappedBy represente le bout Slave
-				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
-				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
-				Departement depManagedEntity = deptRepoistory.findById(depId).get();
-				
+		
+				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).orElse(null);
+				Departement depManagedEntity = deptRepoistory.findById(depId).orElse(null);
+				logger.info("In affecterDepartementAEntreprise method : Departement not affected");
 				depManagedEntity.setEntreprise(entrepriseManagedEntity);
 				deptRepoistory.save(depManagedEntity);
-		
+				logger.info("Out affecterDepartementAEntreprise method : Departement affected ");
 	}
 	
+	@Override
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
-		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).orElse(null);
 		List<String> depNames = new ArrayList<>();
+		logger.info("In getAllDepartementsNamesByEntreprise method");
 		for(Departement dep : entrepriseManagedEntity.getDepartements()){
 			depNames.add(dep.getName());
 		}
-		
+		logger.info("Out getAllDepartementsNamesByEntreprise method");
 		return depNames;
 	}
 
 	@Transactional
+	@Override
 	public void deleteEntrepriseById(int entrepriseId) {
+		try{
 		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());	
+		logger.info("Entreprise deleted successfully");}
+		catch (Exception e) {
+			logger.error("Error in deleteEntrepriseById : " + e );
+		}
 	}
 
 	@Transactional
+	@Override
 	public void deleteDepartementById(int depId) {
-		deptRepoistory.delete(deptRepoistory.findById(depId).get());	
+		deptRepoistory.delete(deptRepoistory.findById(depId).orElse(null));	
+		logger.info("Departement deleted successfully");
 	}
 
-
+	@Override
 	public Entreprise getEntrepriseById(int entrepriseId) {
-		return entrepriseRepoistory.findById(entrepriseId).get();	
+		try{
+		logger.info("getEntrepriseById method");
+		 entrepriseRepoistory.findById(entrepriseId).orElse(null);	}
+		catch (Exception e) {
+			logger.error("Error in getEntrepriseById : " + e );
+		}
+		
+		return entrepriseRepoistory.findById(entrepriseId).orElse(null);	
 	}
 
 }
